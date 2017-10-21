dc36ca902d840be88c769fc1334128fbb8302dd0
MartinBG <MartinBG@abv.bg>
2017-10-21 16:10:42 +0300

    Refactoring and bug-fixing

--- 09 - Strings/homework/edu/homework09/concatenation/Concatenation.java
+++ 09 - Strings/homework/edu/homework09/concatenation/Concatenation.java
@@ -6,30 +6,28 @@ public class Concatenation {
 
 	public static void main(String[] args) {
 		int concatenations = 0;
-		
+
 		System.out.print("Number of string concatenations: ");
 		try (Scanner scanner = new Scanner(System.in)) {
 			concatenations = Integer.parseInt(scanner.nextLine());
 		}
-		
-		long lStartTime = System.nanoTime();
+
+		long timer = System.nanoTime();
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= concatenations; i++) {
 			sb.append(i).append(System.lineSeparator());
 		}
-		long lEndTime = System.nanoTime();
-		long output = lEndTime - lStartTime;
-		System.out.println("[StringBuilder] Elapsed time in milliseconds: " + output / 1000000);
+		timer = System.nanoTime() - timer;
+		System.out.println("[StringBuilder] Elapsed time in milliseconds: " + timer / 1000000);
 
 		// A very slow solution for the problem - concatenating String objects:
-		lStartTime = System.nanoTime();
+		timer = System.nanoTime();
 		String result = "";
 		for (int i = 1; i <= concatenations; i++) {
 			result = result.concat(Integer.toString(i)).concat(System.lineSeparator());
 		}
-		lEndTime = System.nanoTime();
-		output = lEndTime - lStartTime;
-		System.out.println("[string.concat] Elapsed time in milliseconds: " + output / 1000000);
+		timer = System.nanoTime() - timer;
+		System.out.println("[string.concat] Elapsed time in milliseconds: " + timer / 1000000);
 	}
 
 }
--- 09 - Strings/homework/edu/homework09/toupper/ToUpper.java
+++ 09 - Strings/homework/edu/homework09/toupper/ToUpper.java
@@ -1,21 +1,68 @@
 package edu.homework09.toupper;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ToUpper {
 
 	public static void main(String[] args) {
+		int repeats;
+		System.out.print("Number of repeats: ");
+		try (Scanner scanner = new Scanner(System.in)) {
+			repeats = Integer.parseInt(scanner.nextLine());
+		}
+
 		String text = "The code between <upcase> a special tag </upcase> is always uppercase and again <upcase> to upper case </upcase>";
 		String regex = "(?<group><upcase> (?<text>.*?) <\\/upcase>)";
 
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(text);
+		Matcher matcher = null;
 
-		while (matcher.find()) {
-			text = text.replaceFirst(matcher.group("group"), matcher.group("text").toUpperCase());
+		// Using String
+		String newText = null;
+		long timer = System.nanoTime();
+		for (int i = 0; i < repeats; i++) {
+			newText = text;
+			matcher = pattern.matcher(text);
+			while (matcher.find()) {
+				newText = newText.replaceFirst(matcher.group("group"), matcher.group("text").toUpperCase());
+			}
 		}
+		timer = System.nanoTime() - timer;
+		System.out.println("[String] Elapsed time in milliseconds: " + timer / 1000000);
+		System.out.println(newText);
+		System.out.println();
 
-		System.out.println(text);
+		// Using StringBuilder
+		StringBuilder sb = new StringBuilder();
+		timer = System.nanoTime();
+		for (int i = 0; i < repeats; i++) {
+			sb = new StringBuilder(text);
+			matcher = pattern.matcher(sb.toString());
+			while (matcher.find()) {
+				sb.replace(matcher.start(), matcher.end(), matcher.group("text").toUpperCase());
+				matcher = pattern.matcher(sb.toString());
+			}
+		}
+		timer = System.nanoTime() - timer;
+		System.out.println("[StringBuilder] Elapsed time in milliseconds: " + timer / 1000000);
+		System.out.println(sb);
+		System.out.println();
+
+		// Using Matcher
+		timer = System.nanoTime();
+		for (int i = 0; i < repeats; i++) {
+			newText = text;
+			matcher = pattern.matcher(newText);
+			while (matcher.find()) {
+				newText = matcher.replaceFirst(matcher.group("text").toUpperCase());
+				matcher = pattern.matcher(newText);
+			}
+		}
+		timer = System.nanoTime() - timer;
+		System.out.println("[Matcher] Elapsed time in milliseconds: " + timer / 1000000);
+		System.out.println(newText);
 	}
+	
 }
