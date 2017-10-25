4060ee928f3504928fb805a43d63ee067e8f7717
MartinBG <MartinBG@abv.bg>
2017-10-26 01:16:04 +0300

    11 - Enums and MVC

---
+++ 11 - Enums and MVC/homework/dictionary/model/Entry.java
@@ -0,0 +1,21 @@
+package dictionary.model;
+
+public class Entry {
+
+	private String word;
+	private String translation;
+	
+	public Entry(String word, String translation) {
+		super();
+		this.word = word;
+		this.translation = translation;
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	
+	public String getTranslation() {
+		return translation;
+	}
+}
