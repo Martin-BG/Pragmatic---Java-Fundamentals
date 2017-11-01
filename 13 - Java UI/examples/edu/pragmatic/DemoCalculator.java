adefd190e80fe25a43ca9fa263cc3acaf734cd20
MartinBG <MartinBG@abv.bg>
2017-11-01 23:23:14 +0200

    13 - Java UI

---
+++ 13 - Java UI/examples/edu/pragmatic/DemoCalculator.java
@@ -0,0 +1,12 @@
+package edu.pragmatic;
+
+public class DemoCalculator {
+
+	public static void main(String[] args) {
+		
+		CalculatorFrame calculatorFrame = new CalculatorFrame();
+		calculatorFrame.setVisible(true);
+		
+	}
+	
+}
