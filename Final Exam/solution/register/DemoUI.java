e87bfba20357659b1ddda758601a980bcc4c2d86
MartinBG <MartinBG@abv.bg>
2017-11-16 02:24:13 +0200

    Proper variable naming

--- Final Exam/solution/register/DemoUI.java
+++ Final Exam/solution/register/DemoUI.java
@@ -9,8 +9,8 @@ import register.persistance.PersistanceManager;
 public class DemoUI {
 
 	public static void main(String[] args) {
-		RegisterManager registerController = new RegisterManager(PersistanceManager.getInstance(), new History(), new Register(null));
-		RegisterFrame frame = new RegisterFrame(registerController);
+		RegisterManager registerManager = new RegisterManager(PersistanceManager.getInstance(), new History(), new Register(null));
+		RegisterFrame frame = new RegisterFrame(registerManager);
 		frame.setVisible(true);
 	}
 }
